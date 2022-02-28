0e160cc4d57ed8f8696f00e9c8430a555191e5f0
Jun Ageishi <j.ageishi@r-n-i.jp>
2022-02-28 17:49:09 +0900

    リポジトリ名を環境変数に設定

--- run.rb
+++ run.rb
@@ -1,8 +1,8 @@
-require 'octokit'
+repo = ENV['REPOSITORY']
 
 client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
 # タグ一覧を取得する
-tags = client.tags('jageishi/GitHubActionsPlayground')
+tags = client.tags(repo)
 # 最新のreleaseタグを取得する
 latest_release_tag = tags.find{ |t| t.name.start_with?("release") }
 
