f3bab8a32b8d1c76b9c7e1a0778060d9b58c9f14
Jun Ageishi <j.ageishi@r-n-i.jp>
2022-03-01 13:09:34 +0900

    マージコミットとリバーとコミットを取得する

--- run.rb
+++ run.rb
@@ -8,12 +8,17 @@ client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
 tags = client.tags(repo)
 # 最新のreleaseタグを取得する
 latest_release_tag = tags.find { |t| t.name.start_with?('release') }
-# タグ以降のマージコミットを取得する
-merged_commits = []
-client.commits(repo).each do |item|
-  break if item.sha == latest_release_tag.commit.sha
-  merged_commits << item if item.commit.message.start_with?('Merge pull request #')
+# タグ以降のコミットを取得する
+commits = []
+page = 1
+while true
+  temp = client.commits(repo, { :page => page })
+  commits.push(temp)
+  break if commits.any? { |i| i.commit.sha == latest_release_tag.commit.sha }
+  page += 1
 end
+# マージコミットのみを取得する
+merged_commits = commits.filter {|i| i.commit.messsage.start_with?('Merge pull request ') || i.commit.messsage.start_with?('Merge pull request ')}
 
 merged_commits.map do |item|
   puts '----------------------'
